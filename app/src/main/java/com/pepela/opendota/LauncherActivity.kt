fe0d6014edd2845d39bf031a1dfa281da845029b
giorgi.peranidze <giorgi@mooncascade.com>
2018-09-29 19:01:03 +0300

    added launcher activity, added temp logo

---
+++ app/src/main/java/com/pepela/opendota/LauncherActivity.kt
@@ -0,0 +1,14 @@
+package com.pepela.opendota
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        finish()
+    }
+
+}
--- app/src/main/java/com/pepela/opendota/OpenDotaApplication.kt
+++ app/src/main/java/com/pepela/opendota/OpenDotaApplication.kt
@@ -2,6 +2,7 @@ package com.pepela.opendota
 
 import android.app.Application
 import com.pepela.opendota.di.applicationModule
+import com.pepela.remote.player.PlayerServiceFactory
 import org.koin.android.ext.android.startKoin
 import timber.log.Timber
 
